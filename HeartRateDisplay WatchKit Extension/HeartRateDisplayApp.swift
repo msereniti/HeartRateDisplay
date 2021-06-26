c82ff77a21c0862df4fc9badb65714677f69c121
Mikhail Kryuchkov <phytonmk@yandex-team.ru>
2021-06-26 18:34:44 +0300

    Initial Commit

---
+++ HeartRateDisplay WatchKit Extension/HeartRateDisplayApp.swift
@@ -0,0 +1,19 @@
+//
+//  HeartRateDisplayApp.swift
+//  HeartRateDisplay WatchKit Extension
+//
+//  Created by Mikhail Kryuchkov on 6/26/21.
+//
+
+import SwiftUI
+
+@main
+struct HeartRateDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ HeartRateDisplay/HeartRateDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartRateDisplayApp.swift
+//  HeartRateDisplay
+//
+//  Created by Mikhail Kryuchkov on 6/26/21.
+//
+
+import SwiftUI
+
+@main
+struct HeartRateDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
